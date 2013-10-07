5b54a5ee0ac49d5d8e226402ff79d30448c18423
Ian Daniher <it.daniher@gmail.com>
2013-10-07 16:31:58 -0400

    factor integration algorithms out into own library

--- cs1.py
+++ cs1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy
-import rk45
+
+import integrators as qd
 
 from matplotlib import pyplot as plt
 
@@ -8,63 +9,6 @@ from sympy.abc import t
 from sympy import sympify as s
 from sympy import Matrix as M
 
-e = lambda x, c: x.subs(c.items()).evalf()
-
-# forward euler numerical integration
-def feuler(dfdt, x0, v0, ec, c):
-	""" dfdt is a function accepting x, v, and dt, returning a floating point number.
-	x0 is the initial position.
-	v0 is the initial velocity.
-	tn is the final time.
-	c is a dictionary of constants."""
-	x = x0
-	v = v0
-	t = 0
-	to = []
-	xo = []
-	vo = []
-	dt = c['dt']
-	while ec(t, x, v):
-		xo.append(x)
-		to.append(t)
-		vo.append(v)
-		v += dfdt(x,v,dt)*dt
-		x += v*dt
-		t += dt
-	return to, xo, vo
-
-# modified euler numerical integration - trapezoidal approximation
-def meuler(dfdt, x0, v0, ec, c):
-	""" dfdt is a function accepting x, v, and dt,
-		returning either a floating point number or a matrix of floating point numbers.
-	x0 is the initial position.
-	v0 is the initial velocity.
-	tn is the final time.
-	c is a dictionary of constants."""
-	x = x0
-	v = v0
-	t = 0
-	dt = c['dt']
-	tol = c['tol']
-	xo = []
-	to = []
-	vo = []
-	while ec(t, x, v):
-		xo.append(x)
-		to.append(t)
-		vo.append(v)
-		# get left and right estimates, average
-		vi = dfdt(x,v,dt)*dt
-		vf = dfdt(x,vi,dt)*dt
-		v += 0.5*(vi+vf)
-		# calculate error constant
-		er = e(2.0*(vf-vi)/dt**2, c)
-		# evaluate
-		x += v*dt
-		# increment time
-		t += dt
-		dt *= e(sympy.sqrt(tol/(dt*sympy.Abs(vi.norm()-v.norm()))), c)
-	return to, xo, vo
 
 if __name__ == "__main__":
 	# constant terms
@@ -79,27 +23,27 @@ if __name__ == "__main__":
 	# time without drag
 	tf = sympy.solve(1+45*np.sin(np.deg2rad(35)*2)*t+0.5*(-9.8)*t**2,t)[1]
 	# distance without drag
-	xf = e(45*np.cos(np.deg2rad(35.)*2)*t, {"t": tf})
+	xf = qd.e(45*np.cos(np.deg2rad(35.)*2)*t, {"t": tf})
 	print "distance without drag:",  xf
 	print "time without drag:",  tf
 
 	# drag equation
-	dfdt = lambda x, v, dt: e(M([0, s("g")]) + (s("-1/2*p*Cd*A")*v.norm()**2*v.normalized()), c)
+	dfdt = lambda x, v, dt: qd.e(M([0, s("g")]) + (s("-1/2*p*Cd*A")*v.norm()**2*v.normalized()), c)
 	v = M(["vx", "vy"])
-	print "acceleration:", e(M([0, s("g")]) + (s("-1/2*p*Cd*A")*v.norm()**2*v.normalized()), c)
+	print "acceleration:", qd.e(M([0, s("g")]) + (s("-1/2*p*Cd*A")*v.norm()**2*v.normalized()), c)
 	# initial velocity matrix
 	v0 = M([45.*np.cos(np.deg2rad(35.)*2), 45.*np.sin(np.deg2rad(35.)*2)])
 	x0 = M([0, 1])
 	c['dt'] = 0.1
-	c['tol'] = 0.001*xf # 0.1% of estimated range
-	rkres = rk45.rk4(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
+	c['tol'] = 0# 0.001*xf # 0.1% of estimated range
+	rData = qd.rk4(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
 	print 'rk45 complete'
-	meres = meuler(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
+	hData = qd.heun(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
 	print 'modeuler complete'
-	feres = feuler(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
+	eData = qd.euler(dfdt, x0, v0, lambda t, x, v: x[1] > 0, c)
 	print 'fwdeuler complete'
-	plt.plot(rkres[0], rkres[1], marker='o', label='rk45')
-	plt.plot(meres[0], meres[1], marker='.', label='meuler')
-	plt.plot(feres[0], feres[1], marker='v', label='feuler')
+	plt.plot(rData[0], rData[1], marker='o', label='RK4')
+	plt.plot(hData[0], hData[1], marker='.', label='Heun')
+	plt.plot(eData[0], eData[1], marker='v', label='Euler')
 	plt.legend(loc='best')
 	plt.show()
---
+++ integrators.py
@@ -0,0 +1,100 @@
+import numpy as np
+import sympy
+
+from sympy.abc import t
+from sympy import sympify as s
+from sympy import Matrix as M
+
+e = lambda x, c: x.subs(c.items()).evalf()
+
+# forward euler numerical integration
+def euler(dfdt, x, v, ec, c):
+	""" dfdt is a function accepting x, v, and dt, returning a floating point number.
+	x0 is the initial position.
+	v0 is the initial velocity.
+	tn is the final time.
+	c is a dictionary of constants."""
+	t = 0
+	to = []
+	xo = []
+	vo = []
+	dt = c['dt']
+	while ec(t, x, v):
+		xo.append(x)
+		to.append(t)
+		vo.append(v)
+		v += dfdt(x,v,dt)*dt
+		x += v*dt
+		t += dt
+	return to, xo, vo
+
+# heun's method numerical integration - trapezoidal approximation
+def heun(dfdt, x, v, ec, c):
+	""" dfdt is a function accepting x, v, and dt,
+		returning either a floating point number or a matrix of floating point numbers.
+	x0 is the initial position.
+	v0 is the initial velocity.
+	tn is the final time.
+	c is a dictionary of constants."""
+	t = 0
+	dt = c['dt']
+	tol = c['tol']
+	xo = []
+	to = []
+	vo = []
+	while ec(t, x, v):
+		xo.append(x)
+		to.append(t)
+		vo.append(v)
+		# get left and right estimates, average
+		vi = dfdt(x,v,dt)*dt
+		vf = dfdt(x,vi,dt)*dt
+		v += 0.5*(vi+vf)
+		# calculate error constant
+		er = e(2.0*(vf-vi)/dt**2, c)
+		# evaluate
+		x += v*dt
+		# increment time
+		t += dt
+		if tol:
+			dt *= e(sympy.sqrt(tol/(dt*sympy.Abs(vi.norm()-v.norm()))), c)
+	return to, xo, vo
+
+
+def rk4(dfdt, x, v, ec, c = {'dt': 1./40}):
+	dt = c['dt']
+	tol = c['tol']
+	xo = []
+	to = []
+	vo = []
+	t = 0
+	while ec(t, x, v):
+		xo.append(x)
+		to.append(t)
+		vo.append(v)
+		x1 = x
+		v1 = v
+		a1 = dfdt(x1, v1, 0)
+	
+		x2 = x + 0.5*v1*dt
+		v2 = v + 0.5*a1*dt
+		a2 = dfdt(x2, v2, dt/2.0)
+
+		x3 = x + 0.5*v2*dt
+		v3 = v + 0.5*a2*dt
+		a3 = dfdt(x3, v3, dt/2.0)
+		x4 = x + v3*dt
+		v4 = v + a3*dt
+		a4 = dfdt(x4, v4, dt)
+	
+		x = x + (dt/6.0)*(v1 + 2*v2 + 2*v3 + v4)
+		v = v + (dt/6.0)*(a1 + 2*a2 + 2*a3 + a4)
+		t += dt
+		if tol:
+			dt *= e(sympy.sqrt(tol/(dt*sympy.Abs(v2.norm()-v1.norm()))), c)
+	return to, xo, vo
+
+def spring(x, v, dt):
+	stiffness = 1
+	damping = -0.005
+	return -stiffness*x + damping*v
