3d4f7c3dd03c879a0259be22032dd3dc69c1598a
Ian Daniher <it.daniher@gmail.com>
2013-10-24 12:11:28 -0400

    oh yea matrix mul is noncommutative. working newton's method, tho

--- cs5.py
+++ cs5.py
@@ -6,24 +6,24 @@ f = M([[x**2 + y**2 + z**2 - 9], [x*y*z - 1], [x + y - z**2]])
 
 f += M([[x], [y], [z]])
 
-dfdt = f.jacobian([x, y, z])
-print dfdt
-print f
+invdfdt = f.jacobian([x, y, z]).inv()
 
-soln = map(lambda x: map(lambda y: float(y), x), sympy.solve(f, [x,y,z]))[1]
+soln = map(lambda x: map(lambda y: float(y), x), sympy.solve(f, [x,y,z]))
 
-xi = {'x': soln[0]*0.99, 'y': soln[1]*0.99, 'z': soln[2]*0.99 }
+xi = {'x': 0, 'y': 0, 'z': 0}
 
 nSteps = 0
 
-while (float(e(f, xi).norm()) > .1) and (nSteps < 10):
-	plt.plot(nSteps, xi['x'], 'o')
-	xi['x'] -= e(f.row(0)[0], xi)/e(dfdt.row(0)[0], xi).norm()
-	xi['y'] -= e(f.row(0)[1], xi)/e(dfdt.row(0)[1], xi).norm()
-	xi['z'] -= e(f.row(0)[2], xi)/e(dfdt.row(0)[2], xi).norm()
+tol = 0.1
+
+while (float(e(f, xi).norm()) > tol) and (nSteps < 100):
+	dx = e(invdfdt * f, xi)
+	xi['x'] -= dx[0]
+	xi['y'] -= dx[1]
+	xi['z'] -= dx[2]
+	plt.semilogy(nSteps, e(f, xi).norm(), '.')
 	nSteps += 1
-	print xi
+
+print e(f, xi), xi, soln
 
 plt.show()
-
-print e(f, xi), xi
